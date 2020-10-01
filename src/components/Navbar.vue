acb3b64ae3f3288090cc5bb03dec61c082d5d326
zakiyul <zakiyul.py@gmail.com>
2020-10-01 19:48:03 +0800

    toolbars

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <v-toolbar flat class="grey lighten-4">
+      <v-toolbar-title>
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed small>
+        <span class="text-lowercase">Logout</span>
+        <v-icon small right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
