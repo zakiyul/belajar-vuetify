a589b572f94f6a997d840079e6f5edfcfcfac98d
zakiyul <zakiyul.py@gmail.com>
2020-10-01 21:37:14 +0800

    navigation drawers

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,11 +5,24 @@
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer class="hidden-sm-and-down"></v-spacer>
       <v-btn depressed small>
         <span class="text-lowercase">Logout</span>
         <v-icon small right>exit_to_app</v-icon>
       </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-toolbar>
+    <v-navigation-drawer app v-model="drawer" class="purple">
+      <p class="white--text">test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
