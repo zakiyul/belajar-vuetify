cbed2cfa19ba22a1fd000cc12219b54f453631d7
zakiyul <zakiyul.py@gmail.com>
2020-10-02 11:20:46 +0800

    lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,23 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-toolbar>
     <v-navigation-drawer app v-model="drawer" class="primary">
-      <p class="white--text">test</p>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -22,6 +38,11 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" },
+      ],
     };
   },
 };
